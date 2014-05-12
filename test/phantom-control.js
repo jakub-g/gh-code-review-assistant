5bda3dfca4c69b1124a5a9c16bdfe782e3549e32
jakub-g <jakub.g.opensource@gmail.com>
2014-05-12 15:20:17 +0200

    test WIP

--- test/phantom-control.js
+++ test/phantom-control.js
@@ -1,63 +1,43 @@
-var url = "https://github.com/ariatemplates/ariatemplates/pull/1117/files";
-//var url = "http://topspot/index.php/Aria_Templates_-_Deprecated_features";
+var webpage = require('webpage');
+
 var userScriptPath = "../ghAssistant.user.js";
 
-var page = require('webpage').create();
+function openAndTest (url, testCb) {
+    var page = webpage.create();
+    page.onConsoleMessage = function (msg) {
+        console.log('>>> ', msg);
+    };
 
-page.onInitialized = function() {
-    page.evaluate(function () {
-        document.addEventListener("DOMContentLoaded", function () {
-            window.callPhantom({
-                name: "DOMContentLoaded"
-            });
-        }, false);
+    page.onError = function(msg, trace) {
+      var msgStack = ['PHANTOM ERROR: ' + msg];
+      if (trace && trace.length) {
+        trace.forEach(function(t) {
+          msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
+        });
+        msgStack.push("");
+      }
+      console.error(msgStack.join('\n'));
+    };
+
+    page.open(url, function (status) {
+        console.log("Loaded " + url + " with status " + status);
+
+        console.log("Injecting the userscript... ");
+        var injected = page.injectJs(userScriptPath);
+        console.log(injected ? "OK" : "FAILED");
+
+        page.evaluate(tests);
+
+        phantom.exit();
     });
-};
-
-page.onCallback = function (event) {
-    if (event.name == "DOMContentLoaded") {
-        onDOMContentLoaded();
-    }
-};
-
-page.onConsoleMessage = function (msg) {
-    console.log('>>> ', msg);
-};
-
-page.onError = function(msg, trace) {
-  var msgStack = ['PHANTOM ERROR: ' + msg];
-  if (trace && trace.length) {
-    trace.forEach(function(t) {
-      msgStack.push(' -> ' + (t.file || t.sourceURL) + ': ' + t.line + (t.function ? ' (in function ' + t.function +')' : ''));
-    });
-    msgStack.push("");
-  }
-  console.error(msgStack.join('\n'));
-};
-
-page.open(url, function (status) {
-    console.log("Loaded " + url + " with status " + status);
-
-    page.evaluate(function(){
-    var len = document.querySelectorAll('.ghaCfgOpenButton').length;
-    console.log(len);
-    });
-    
-    phantom.exit();
-});
-
-function onDOMContentLoaded () {
-    console.log("DOMContentLoaded");
-    var injected = page.injectJs(userScriptPath);
-    console.log("injected? " + injected);
-    page.evaluate(tests);
 }
 
 // ==================================================================================================
 
-function tests () {
+var url = "https://github.com/ariatemplates/ariatemplates/pull/1117/files";
+openAndTest(url, function () {
     var elm = document.querySelector('.js-issue-title').innerText;
     console.log(elm);
     var len = document.querySelectorAll('.ghaCfgOpenButton').length;
     console.log(len);
-}
+});
