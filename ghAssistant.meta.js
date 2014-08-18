c64070961b77ea7b5ffc748e5cf89ec12ce2413c
jakub-g <jakub.g.opensource@gmail.com>
2014-08-18 22:50:03 +0200

    1.0.3

--- ghAssistant.meta.js
+++ ghAssistant.meta.js
@@ -2,11 +2,11 @@
 // @name            GitHub code review assistant
 // @description     Collapse & expand files one by one on diffs and mark them as reviewed. Useful to review commits with lots of files changed.
 // @icon            https://github.com/favicon.ico
-// @version         1.0.2.20140724
+// @version         1.0.3.20140818
 // @namespace       http://jakub-g.github.com/
 // @author          http://jakub-g.github.com/
-// @downloadURL     https://raw.github.com/jakub-g/gh-code-review-assistant/master/ghAssistant.user.js
-// @updateURL       https://raw.github.com/jakub-g/gh-code-review-assistant/master/ghAssistant.meta.js
+// @downloadURL     https://raw.githubusercontent.com/jakub-g/gh-code-review-assistant/master/ghAssistant.user.js
+// @updateURL       https://raw.githubusercontent.com/jakub-g/gh-code-review-assistant/master/ghAssistant.meta.js
 // @userscriptsOrg  http://userscripts.org/scripts/show/153049
 // @grant           GM_getValue
 // @grant           GM_setValue
