d85ec5c794773890946f3da13c5c6d98853efff4
jgieryluk <jakub.gieryluk@amadeus.com>
2015-03-24 17:09:26 +0100

    1.0.5

--- ghAssistant.meta.js
+++ ghAssistant.meta.js
@@ -2,7 +2,7 @@
 // @name            GitHub code review assistant
 // @description     Collapse & expand files one by one on diffs and mark them as reviewed. Useful to review commits with lots of files changed.
 // @icon            https://github.com/favicon.ico
-// @version         1.0.4.20150226
+// @version         1.0.5.20150324
 // @namespace       http://jakub-g.github.com/
 // @author          http://jakub-g.github.com/
 // @downloadURL     https://raw.githubusercontent.com/jakub-g/gh-code-review-assistant/master/ghAssistant.user.js
